e12cdfa1176494205ec0dd60794ce06ce33aa6ca
AlbertoGiovannoni <alberto.giovannoni@edu.unifi.it>
2024-03-14 10:00:19 +0100

    Sequential: implemented naive and optimized               version Parallel: initialization

--- parallel.cu
+++ parallel.cu
@@ -1,10 +1,83 @@
 #include <stdio.h>
+#include <cuda_runtime.h>
 
-__global__ void hello(){
+
+__global__ void hello() {
     printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
 }
 
-int main(){
-    hello<<<2, 2>>>();
-    cudaDeviceSynchronize();
+int getSPcores(cudaDeviceProp devProp) {
+    int cores = 0;
+    int mp = devProp.multiProcessorCount;
+    switch (devProp.major) {
+        case 2: // Fermi
+            if (devProp.minor == 1) cores = mp * 48;
+            else cores = mp * 32;
+            break;
+        case 3: // Kepler
+            cores = mp * 192;
+            break;
+        case 5: // Maxwell
+            cores = mp * 128;
+            break;
+        case 6: // Pascal
+            if ((devProp.minor == 1) || (devProp.minor == 2)) cores = mp * 128;
+            else if (devProp.minor == 0) cores = mp * 64;
+            else printf("Unknown device type\n");
+            break;
+        case 7: // Volta and Turing
+            if ((devProp.minor == 0) || (devProp.minor == 5)) cores = mp * 64;
+            else printf("Unknown device type\n");
+            break;
+        case 8: // Ampere
+            if (devProp.minor == 0) cores = mp * 64;
+            else if (devProp.minor == 6) cores = mp * 128;
+            else if (devProp.minor == 9) cores = mp * 128; // ada lovelace
+            else printf("Unknown device type\n");
+            break;
+        case 9: // Hopper
+            if (devProp.minor == 0) cores = mp * 128;
+            else printf("Unknown device type\n");
+            break;
+        default:
+            printf("Unknown device type\n");
+            break;
+    }
+    return cores;
+}
+
+int* createImage(int* img, int width, int height){
+    for (int i = 0; i < width * height; ++i) {
+        img[i] = i + 1;
+    }
+    return img;
+}
+
+int main() {
+    int height = 100;
+    int width = 100;
+    bool printOutput = false;
+    int imgSize = height * width * sizeof(int);
+    int integImgSize = height * width * sizeof(int);
+
+    // Stampa delle specifiche hardware
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, 0);
+    printf("Device: %s\n", deviceProp.name);
+    printf("Cores number: %d\n", getSPcores(deviceProp));
+
+    // Allocazione memoria CPU
+    int* cpu_img = (int*) malloc(imgSize);
+    cpu_img = createImage(cpu_img, width, height);
+
+    // Allocazione memoria GPU
+    int* gpu_img;
+    int* gpu_integImg;
+    cudaMalloc((void**)&gpu_img, imgSize);
+    cudaMalloc((void**)&gpu_integImg, integImgSize);
+
+    // Copia dei dati da cpu a gpu
+    cudaMemcpy(gpu_img, cpu_img, imgSize, cudaMemcpyHostToDevice);
+
+
 }
--- sequential.cpp
+++ sequential.cpp
@@ -1,51 +1,108 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
-void imagePrint(const std::vector<std::vector<int>>& image) {
-    int height = image.size();
-    int width = image[0].size();
-
+void imagePrint(int* image, int width, int height) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            std::cout << image[i][j] << " ";
+            std::cout << image[i * width + j] << " ";
         }
         std::cout << std::endl;
     }
 }
-std::vector<std::vector<int>> createImage(int height, int width){
-    std::vector<std::vector<int>> image;
-    int c = 1;
 
-    for (int i = 0; i < height; i++) {
-        std::vector<int> row;
-        for (int j = 0; j < width; j++) {
-            row.push_back(c);
-            c++;
-        }
-        image.push_back(row);
+int* createImage(int height, int width) {
+    int* image = (int*)malloc(width * height * sizeof(int));
+    for (int i = 0; i < width * height; i++) {
+        image[i] = i + 1;
     }
-
     return image;
 }
 
-std::vector<std::vector<int>> integralImage(std::vector<std::vector<int>> image){
-    int height = image.size();
-    int width = image[0].size();
-    std::vector<std::vector<int>> integImg(height, std::vector<int>(width, 0));
+int* integralImage_naive(int* image, int width, int height) {
+    int* integImg = (int*)malloc(width * height * sizeof(int));
 
+    for (int h = 0; h < height; h++) {
+        for (int w = 0; w < width; w++) {
+            int sum = 0;
+            for (int j = 0; j <= h; j++) {
+                for (int i = 0; i <= w; i++) {
+                    sum += image[j * width + i];
+                }
+            }
+            integImg[h * width + w] = sum; // Assegna il valore della somma all'immagine integrale
+        }
+    }
+
+    return integImg;
+}
+
+int* integralImage_optimized(int* image, int* integImg, int width, int height) {
+
+    // Calcolo la prima riga
+    integImg[0] = image[0];
+    for (int i = 1; i < width; i++)
+        integImg[i] = integImg[i - 1] + image[i];
+
+    // Calcolo la prima colonna
+    for (int i = 1; i < height; i++)
+        integImg[i * width] = integImg[(i - 1) * width] + image[i * width];
+
+    // Calcolo il resto
+    for (int i = 1; i < height; i++) {
+        for (int j = 1; j < width; j++) {
+            integImg[i * width + j] = (integImg[(i - 1) * width + j] +
+                                       integImg[i * width + j - 1] -
+                                       integImg[(i - 1) * width + j - 1] +
+                                       image[i * width + j]);
+        }
+    }
 
     return integImg;
 }
 
 int main() {
-    int height = 10;
-    int width = 10;
+    int height = 3;
+    int width = 3;
+    bool printOutput = true;
+    bool naive = true;
 
-    std::vector<std::vector<int>> image = createImage(height, width);
-    imagePrint(image);
+    int* image = createImage(height, width);
+    int* integImg = (int*)malloc(width * height * sizeof(int));
+    int* integImg_opt = (int*)malloc(width * height * sizeof(int));
 
-    std::vector<std::vector<int>> integImg = integralImage(image);
-    imagePrint(integImg);
+    if (printOutput) {
+        std::cout << "Immagine di input" << std::endl;
+        imagePrint(image, width, height);
+        std::cout << "\n" << std::endl;
+    }
+
+    if (naive) {
+        std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+        integImg = integralImage_naive(image, width, height);
+        std::chrono::duration<double> totalTime = std::chrono::steady_clock::now() - start;
+
+        if (printOutput) {
+            std::cout << "Output versione naive" << std::endl;
+            imagePrint(integImg, width, height);
+        }
+        std::cout << "Tempo versione naive: " << totalTime.count() << " secondi\n" << std::endl;
+    }
+
+    std::chrono::steady_clock::time_point start_opt = std::chrono::steady_clock::now();
+    integImg_opt = integralImage_optimized(image, integImg_opt, width, height);
+    std::chrono::duration<double> totalTime_opt = std::chrono::steady_clock::now() - start_opt;
+    if (printOutput) {
+        std::cout << "Output versione ottimizzata" << std::endl;
+        imagePrint(integImg_opt, width, height);
+    }
+
+    double milliseconds_opt = totalTime_opt.count() * 1000.0;
+    std::cout << "Tempo versione ottimizzata: " << milliseconds_opt << " millisecondi" << std::endl;
+
+    free(image);
+    free(integImg);
+    free(integImg_opt);
 
     return 0;
 }
